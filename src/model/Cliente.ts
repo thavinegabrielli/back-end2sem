1871c491ba2089d5347b69f541d6e088523e5d74
Thávine Gabrielli Nardin <kimjojo542@gmail.com>
2024-10-08 16:31:00 -0300

    Iniciando o projeto, criando os arquivos index.html e estilo.css

--- src/model/Cliente.ts
+++ src/model/Cliente.ts
@@ -1,3 +1,28 @@
+/**
+ * Classe Cliente que representa um cliente com atributos de identificação pessoal.
+ * 
+ * A classe inclui métodos para definir e obter os valores dos atributos `nome`, `cpf`, `telefone` e `idCliente`.
+ * Também possui uma função estática `listagemClientes` que recupera todos os clientes do banco de dados.
+ * 
+ * @param {string} nome - Nome do cliente.
+ * @param {string} cpf - CPF do cliente.
+ * @param {string} telefone - Telefone do cliente.
+ * 
+ * Métodos:
+ * - setIdCliente: Define o ID do cliente.
+ * - getIdCliente: Retorna o ID do cliente.
+ * - setNome: Define o nome do cliente.
+ * - getNome: Retorna o nome do cliente.
+ * - setCpf: Define o CPF do cliente.
+ * - getCpf: Retorna o CPF do cliente.
+ * - setTelefone: Define o telefone do cliente.
+ * - getTelefone: Retorna o telefone do cliente.
+ * 
+ * Função estática:
+ * - listagemClientes: Função assíncrona que busca todos os clientes no banco de dados,
+ *   instancia objetos da classe Cliente e retorna uma lista de clientes.
+ *   Em caso de erro, retorna `null`.
+ */
 import { DatabaseModel } from "./DatababeModel";
 
 const database = new DatabaseModel().pool;
@@ -22,9 +47,35 @@ export class Cliente {
         return this.idCliente
     }
 
-    // Lista de Clientes
+
+    public getNome(): string {
+        return this.nome
+    }
+
+    public setNome(nomeCliente: string): void {
+        this.nome = nomeCliente;
+    }
+
+
+    public setCpf(cpfCliente: string): void {
+        this.cpf = cpfCliente;
+    }
+
+    public getCpf(): string {
+        return this.cpf
+    }
+
+    public setTelefone(telefoneCliente: string): void {
+        this.telefone = telefoneCliente;
+    }
+
+    public getTelefone(): string {
+        return this.telefone
+    }
+
+
     static async listagemClientes(): Promise<Array<Cliente> | null> {
-        let listaDePessoas: Array<Cliente> = [];
+        let listaDeClientes: Array<Cliente> = [];
 
         try {
             //query para consultar no banco de dados
@@ -44,11 +95,11 @@ export class Cliente {
                 novoCliente.setIdCliente(cliente.id_cliente);
 
                 //adicionando a pessoa na lista 
-                listaDePessoas.push(novoCliente);
+                listaDeClientes.push(novoCliente);
             })
 
             //retornaqndo a lista de pessoas para que chamou a função
-            return listaDePessoas;
+            return listaDeClientes;
 
         } catch (error) {
             console.log(`Erro ao acessar o modelo:${error}`);
--- src/model/PedidoVenda.ts
+++ src/model/PedidoVenda.ts
@@ -1,3 +1,29 @@
+/**
+ * Classe PedidoVenda que representa um pedido de venda com atributos relacionados ao pedido.
+ * 
+ * A classe inclui métodos para definir e obter os valores dos atributos `idPedido`, `idCarro`, `idCliente`, 
+ * `dataPedido` e `valorPedido`. Também possui um método para listar pedidos de venda.
+ * 
+ * @param {number} idPedido - ID do pedido de venda.
+ * @param {number} idCarro - ID do carro relacionado ao pedido.
+ * @param {number} idCliente - ID do cliente que realizou o pedido.
+ * @param {Date} dataPedido - Data em que o pedido foi realizado.
+ * @param {number} valorPedido - Valor total do pedido.
+ * 
+ * Métodos:
+ * - setIdPedido: Define o ID do pedido.
+ * - getIdPedido: Retorna o ID do pedido.
+ * - setIdCarro: Define o ID do carro.
+ * - getIdCarro: Retorna o ID do carro.
+ * - setIdCliente: Define o ID do cliente.
+ * - getIdCliente: Retorna o ID do cliente.
+ * - listagemPedidos: Retorna uma lista de objetos PedidoVenda ou null.
+ */
+
+import { DatabaseModel } from "./DatababeModel";
+
+const database = new DatabaseModel().pool;
+
 export class PedidoVenda {
     idPedido: number;
     idCarro: number;
@@ -12,30 +38,34 @@ export class PedidoVenda {
         this.dataPedido = dataPedido;
         this.valorPedido = valorPedido;
     }
+    public setIdPedido(idPedido: number): void {
+        this.idPedido = idPedido;
+    }
+
+    public getIdPedido(): number {
+        return this.idPedido
+    }
+
+    public setIdCarro(idCarro: number): void {
+        this.idCarro = idCarro;
+    }
+
+    public getIdCarro(): number {
+        return this.idCarro
+    }
+
+    public setIdCliente(idCliente: number): void {
+        this.idCarro = idCliente;
+    }
+
+    public getIdCliente(): number {
+        return this.idCliente
+    }
+
 
     // Lista de Pedidos
-    listagemPedidos(): Array<PedidoVenda> {
+    listagemPedidos(): Array<PedidoVenda> | null {
         // Lógica para listar pedidos
         return [];
-    }
-
-    // Cadastra um novo pedido
-    cadastroPedido(idCliente: number, idCarro: number, dataPedido: Date, valorPedido: number): boolean {
-        // Lógica para cadastrar um novo pedido
-        return true;
-    }
-
-    // Remove um pedido
-    removerPedido(idPedido: number): boolean {
-        // Lógica para remover um pedido
-        return true;
-    }
-
-    // Atualiza um pedido existente
-    atualizarPedido(idPedido: number, idCliente: number, idCarro: number, 
-        dataPedido: Date, valorPedido: number): boolean 
-        {   
-        // Lógica para atualizar um pedido
-        return true;
     }
 }
